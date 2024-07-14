5ea4cfb9a73732b37e3482a5d83aab77dcb2ce48
Daniyar Bazakov <bazakovd@gmail.com>
2024-07-14 11:57:38 -0400

    render tweet with JQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,93 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 240000000
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+$(document).ready(function() {
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    for (let tweet of tweets) {
+      // calls createTweetElement for each tweet
+      // takes return value and appends it to the tweets container
+      $('.tweet-container').append(createTweetElement(tweet));
+    }
+  };
+
+  const timeSince = (date) => {
+    const seconds = Math.floor((new Date() - date) / 1000);
+  
+    let interval = seconds / 31536000;
+    if (interval > 1) {
+      return Math.floor(interval) + " years ago";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + " months ago";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + " days ago";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + " hours ago";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + " minutes ago";
+    }
+    return Math.floor(seconds) + " seconds ago";
+  };
+  
+  const createTweetElement = function(tweet) {
+    const tweetDate = new Date(tweet.created_at);
+    const formattedDate = timeSince(tweetDate);
+    const $tweet = $(`
+      <article class="tweet">
+        <header>
+          <div class="logo-username">
+            <img src="${tweet.user.avatars}" alt="Avatar" class="tweet-avatar">
+            <h3>${tweet.user.name}</h3>
+          </div>
+          <h3 class="tweet-account">${tweet.user.handle}</h3>
+        </header>
+        <p class="tweet-text">${tweet.content.text}</p>
+        <footer>
+          <time>${formattedDate}</time>
+          <div class="footer-icons">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+    `);
+    return $tweet;
+  };
+
+  renderTweets(data);
+});
 
